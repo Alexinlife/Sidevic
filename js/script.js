ce291bdb1e53a20e89578c726a92634107b01ced
Alexinlife <alexinlife.exe@gmail.com>
2019-05-26 17:59:10 -0400

    Publication du site

---
+++ js/script.js
@@ -0,0 +1,5 @@
+if ($(window).width() < 600) {
+    $("#description").html("Que vous soyez un particulier ou bien une entreprise, que vous recherchiez de l’outillage professionnel ou des fournitures industrielles, nous sommes la référence dans ces domaines depuis 1981.<br>\n" +
+        "Avec plus de 100 lignes de produits différents et un inventaire en boulonnerie des plus complètes en région, nous sommes prêts à vous conseiller.<br>\n" +
+        "Merci de nous faire confiance!");
+}
